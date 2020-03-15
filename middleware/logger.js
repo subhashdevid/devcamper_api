c08352da17c2ee7f53f63c253c20f466aa1ae5b2
Subhash Kumar <subhashdevid@gmail.com>
2020-03-15 14:35:40 +0530

    middleware added

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+
+// @desc  Request log to server.js
+
+const logger = (req, res, next) =>{
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+next();
+}
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require('morgan');
 
 // Route File
 const bootcamps = require('./routes/bootcamps');
+
 //load env var
 dotenv.config({ path: "./config/config.env" });
 const app = express();
 const PORT = process.env.PORT || 6000;
 
+//dev logging env
+
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
 // Mount router
 app.use('/api/v1/bootcamps',bootcamps);
 
